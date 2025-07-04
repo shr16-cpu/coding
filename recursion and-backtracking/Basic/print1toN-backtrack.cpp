03ce61b7b81204e0232af332d7318722e50a9a0d
shraddha <gendle.shraddha16@gmail.com>
2025-07-04 07:50:10 +0530

    adding code for printing N to 1 with backtracking

---
+++ recursion and-backtracking/Basic/print1toN-backtrack.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void print1toN(int n){
+  if(n<1){
+    return;
+  }
+  print1toN(n-1);
+  cout << n << " ";
+}
+int main(){
+  int n;
+  cin >> n;
+  print1toN(n);
+  return 0;
+}
